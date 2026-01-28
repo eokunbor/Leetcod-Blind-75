78b2811a73390c279199ef92c9d94149fa31be55
Elizabeth Okunbor <elizabethokunbor04@gmail.com>
2026-01-27 21:25:10 -0600

    Problem 217

---
+++ 217. Contains_Duplicate/Submission_1.py
@@ -0,0 +1,8 @@
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        length = len(nums)
+        for i in range(length):
+            for j in range(i + 1, length):
+                if nums[i] == nums[j]:
+                    return True
+        return False
